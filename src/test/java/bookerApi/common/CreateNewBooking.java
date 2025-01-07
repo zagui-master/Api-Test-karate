3b02cfdc3e154a14763b70464f2c7cb35a8fe404
ZaguiMaster <github.correo@gmail.com>
2025-01-07 17:59:03 -0500

    Se le cambió el nombre de la clase del método del runner

--- src/test/java/bookerApi/common/CreateNewBooking.java
+++ src/test/java/bookerApi/common/CreateNewBooking.java
@@ -2,10 +2,10 @@ package bookerApi.common;
 
 import com.intuit.karate.junit5.Karate;
 
-public class GetRunner {
+public class CreateNewBooking {
 
     @Karate.Test
-    Karate testGetBooking() {
+    Karate testCreateNewBookingBooking() {
         return Karate.run().relativeTo(getClass()).tags("~@ignore");
     }
 
