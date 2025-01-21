308652168f9cf0dbd0f85e5a951d6aa4ac08d34b
ZaguiMaster <github.correo@gmail.com>
2025-01-21 08:54:16 -0500

    Creación de la clase encargada de generar los reportes

--- src/test/java/bookerApi/TestRunner.java
+++ src/test/java/bookerApi/TestRunner.java
@@ -1,19 +1,41 @@
 package bookerApi;
 
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+//import report.GenerateReport;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
 
 public class TestRunner {
     @Test
     void testParallel() {
-        Results results = Runner.path("classpath:bookerApi").tags("~ignore")
-                //.outputCucumberJson(true)
-                .parallel(5);
+        Results results =
+                Runner.path("classpath:bookerApi")
+                        .tags("~ignore")
+                        .outputCucumberJson(true)
+                        .parallel(Runtime.getRuntime().availableProcessors());
+
+        // GenerateReport.main(new String[]{});
+        generateReport(results.getReportDir());
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 }
